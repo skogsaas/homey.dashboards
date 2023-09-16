daba2eea4fcf0280bd4c69a8884297a259b5aed5
Marcus Wærsted Skogsaas <marcus@skogsaas.no>
2023-09-16 18:48:29 +0200

    Add Sentry for error logging with some filters to enhance privacy

---
+++ web/src/routes/sentry.ts
@@ -0,0 +1,34 @@
+import { version } from "$app/environment";
+import * as Sentry from "@sentry/svelte";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    new Sentry.BrowserTracing(), 
+    new Sentry.Replay({
+      maxReplayDuration: 60000,
+      beforeAddRecordingEvent: (event) => {
+        if (
+          event.data.tag === "performanceSpan" && 
+          (
+            event.data.payload.op === "resource.fetch" || 
+            event.data.payload.op === "resource.xhr" || 
+            event.data.payload.op === "resource.img"
+          ) &&
+          event.data.payload.data?.statusCode < 400
+        ) {
+          return null;
+        }
+    
+        return event;
+      },
+    }),
+  ],
+
+  release: version,
+
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost"],
+  replaysSessionSampleRate: 0.0,
+  replaysOnErrorSampleRate: 1.0,
+});
