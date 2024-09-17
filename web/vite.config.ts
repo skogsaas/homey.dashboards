479698dbb383fbb3e0c07127a3d630203d213077
Marcus Wærsted Skogsaas <marcus@skogsaas.no>
2024-09-17 19:26:59 +0200

    Update manifest icons

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -21,18 +21,28 @@ export default defineConfig({
 				background_color: "#ffffff",
 				icons: [
 					{
-						src: '/small.png',
-						sizes: '250x175',
+						src: '/512.png',
+						sizes: '512x512',
 						type: 'image/png',
 					},
 					{
-						src: '/large.png',
-						sizes: '500x350',
+						src: '/256.png',
+						sizes: '256x256',
 						type: 'image/png',
 					},
 					{
-						src: '/xlarge.png',
-						sizes: '1000x700',
+						src: '/128.png',
+						sizes: '128x128',
+						type: 'image/png',
+					},
+					{
+						src: '/64.png',
+						sizes: '64x64',
+						type: 'image/png',
+					},
+					{
+						src: '/32.png',
+						sizes: '32x32',
 						type: 'image/png',
 					},
 				],
