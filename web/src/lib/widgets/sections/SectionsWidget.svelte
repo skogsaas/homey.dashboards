fbc61a0c638f46c2ce9b3316388abd3a35cde904
Marcus Wærsted Skogsaas <marcus@skogsaas.no>
2024-12-19 10:46:35 +0100

    feat(sections-widget): Add support for adjusting number of columns and gaps

--- web/src/lib/widgets/sections/SectionsWidget.svelte
+++ web/src/lib/widgets/sections/SectionsWidget.svelte
@@ -16,10 +16,38 @@
     const dispatch = createEventDispatcher();
 
     let items: WidgetSettings_v1[];
+    
+    let sm: string;
+    let smGap: string;
+
+    let md: string;
+    let mdGap: string;
+
+    let lg: string;
+    let lgGap: string;
+
+    let xl: string;
+    let xlGap: string;
+
+    let xxl: string;
+    let xxlGap: string;
+
     $: onSettings(settings);
 
     function onSettings(_settings: SectionsSettings_v1) {
         items = settings.items ?? [];
+
+        sm = settings.sm ?? 'grid-cols-1';
+        md = settings.md ?? 'grid-cols-2';
+        lg = settings.lg ?? 'grid-cols-3';
+        xl = settings.xl ?? 'grid-cols-4';
+        xxl = settings.xxl ?? 'grid-cols-5';
+
+        smGap = settings.smGap ?? 'gap-2';
+        mdGap = settings.mdGap ?? 'gap-2';
+        lgGap = settings.lgGap ?? 'gap-2';
+        xlGap = settings.xlGap ?? 'gap-2';
+        xxlGap = settings.xxlGap ?? 'gap-2';
     }
 
     function onItems(_items: WidgetSettings_v1[]) {
@@ -62,7 +90,7 @@
     items={items}
     on:items={e => onItems(e.detail)} 
     editable={context.editable}
-    class="w-full grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 {context.editable ? 'min-h-[50px]' : ''}" 
+    class="w-full grid gap-1 sm:{smGap} md:{mdGap} lg:{lgGap} xl:{xlGap} 2xl:{xxlGap} grid-cols-1 sm:{sm} md:{md} lg:{lg} xl:{xl} 2xl:{xxl} {context.editable ? 'min-h-[50px]' : ''}" 
     let:item
 >
     <Widget {context} settings={item} on:settings={e => updateSection(e.detail)} />
